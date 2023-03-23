bb3d385e8fe06b49f496c4ed601d897f538c295c
Aviles79 <127035005+Aviles79@users.noreply.github.com>
2023-03-22 19:05:56 -0600

    Add files via upload

---
+++ Introduccion a dark/ejercicio4.dart
@@ -0,0 +1,39 @@
+import 'dart:math';
+
+abstract class Shape {
+ 
+  factory Shape(String type) {  
+    if (type == 'square') return Square(2);
+    throw 'Can\'t create $type.';
+  }
+  
+  num get area;
+}
+class Circle implements Shape {
+  final num radius;
+  Circle(this.radius);
+  num get area => pi * pow(radius, 2);
+}
+
+
+class Square implements Shape {
+  
+  final num side;
+  Square(this.side);
+  
+}
+
+
+class CircleMock implements Circle {
+  
+  num area = 0;
+   num radius = 0;
+}
+
+main() {
+ 
+  final circle = Shape('circle');
+  final square = Shape('square');
+  print(circle.area);
+  print(square.area);
+}
